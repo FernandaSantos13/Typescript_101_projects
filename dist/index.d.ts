a83411dce210dd361d0be0aebed2e26302a4b433
FernandaSantos13 <fernandacsantos.bio@gmail.com>
2024-10-02 13:10:47 -0400

    Removing node_modules and dist from version control

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare let givers: string[];
-interface SecretSantaList {
-    giver: string;
-    receiver: string;
-}
-declare let secretSantaList: SecretSantaList[];
-declare function drawInteger(length: number): number;
-declare let givers2: string[];
