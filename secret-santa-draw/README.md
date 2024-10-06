f8fd4158221faa03c3e95e085ab255140b0e1d04
FernandaSantos13 <fernandacsantos.bio@gmail.com>
2024-10-06 00:43:34 -0400

    Includes the optimized solution suggested by Marvio.

--- secret-santa-draw/README.md
+++ secret-santa-draw/README.md
@@ -2,4 +2,4 @@
 
  The Secret Santa Coding Challenge (secret-santa-draw) is a program that randomly assigns each participant in a list to another participant as their Secret Santa. Each participant should give a gift to exactly one other participant and receive a gift from exactly one other participant.
 
- Type the list of participants in the file src/index.ts (line 12) to create an array called givers.
+ Type the list of participants in the file src/index.ts (line 38) to create an array called givers01.
--- secret-santa-draw/src/index.ts
+++ secret-santa-draw/src/index.ts
@@ -1,56 +1,34 @@
 // The secret santa challenge
-/* 1- Receives an array with names
-2- Create an object array containing properties giver and receiver. Receivers will be assigned from loop 
-3- Creates a function loop that iterate in each name of givers, assings a value to giver accordign to i,
-gets the list of givers, remove the givers[i] and save to temp
-4- sort a number from 0 to temp.length()-1 and save to drawedNum, receiverwill receive temp[num]. 
-The loop ends by creating the array givers2 containing all individuals in givers except for the one 
-assigned as receiver.
-5- The loop stops when i<givers.length-2 (2 individuals not yet assigned as receivers) to avoid 
-undefined values. 
+/* 1- Receives an array with names givers01
+2- Uses a function to shuffle names in the list and later it usesa second function to assign
+each participant in the shuffled list to the one in the next position. 
  */
-let givers= ['Andre', 'Brad', 'Claudia', 'Denise', 'Emily','Fabby'];
-interface SecretSantaList {
-    giver: string;
-    receiver: string;
-}
-let secretSantaList:  SecretSantaList[] = [];
+const shuffle = (input: string[]) => {
+    const result = [...input];
 
-function drawInteger(length: number) {
-    return Math.floor(Math.random() * length); // Generates a number between 0 and 5
-}
-
-
-let givers2=[...givers];
-for (let i=0; i < givers.length-2; i++) {
-    let temp=[...givers2];
-    let j=temp.indexOf(givers[i])
-        if (j > -1) {
-            temp.splice(j,1); 
-            };
-    let size= temp.length;
-    let drawedNum=drawInteger(size);
-    secretSantaList.push({ giver: givers[i], receiver: temp[drawedNum]});
-    let index= givers2.indexOf(temp[drawedNum]);
-    givers2.splice(index,1);
-}
-
-if(givers[givers.length-1]==givers2[1]){
-    secretSantaList.push({ giver: givers[givers.length-1], receiver: givers2[0]});
-    secretSantaList.push({ giver: givers[givers.length-2], receiver: givers2[1]}); 
-} else {
-    for (let i=givers.length-2; i < givers.length; i++) {
-    let temp=[...givers2];
-    let j=temp.indexOf(givers[i]);
-        if (j > -1) {
-            temp.splice(j,1); 
-            };
-    let size= temp.length;
-    let drawedNum=drawInteger(size);
-    secretSantaList.push({ giver: givers[i], receiver: temp[drawedNum]});
-    let index= givers2.indexOf(temp[drawedNum]);
-    givers2.splice(index,1);
+    for (let i = 0; i < result.length; i++) {
+        const j = Math.floor(Math.random() * result.length);
+        const temp = result[i];
+        result[i] = result[j];
+        result[j] = temp;
     }
+
+    return result;
 }
 
-console.log(secretSantaList);
+const main = (input: string[]) => {
+    const shuffled = shuffle(input);
+
+    const result = shuffled.map((value, index) => {
+        return {
+            giver: value,
+            receiver: shuffled[index + 1] || shuffled[0]
+        }
+    });
+
+    console.log(result);
+}
+
+const givers01 = ['Andre', 'Brad', 'Claudia', 'Denise', 'Emily', 'Fabby'];
+
+main(givers01);
