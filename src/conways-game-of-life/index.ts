05c40ce35d4ab8d3cc9169cac9483aff65e9b1f6
FernandaSantos13 <fernandacsantos.bio@gmail.com>
2024-10-23 16:56:36 -0400

    Implementing more meaningful variable names.

--- src/conways-game-of-life/index.ts
+++ src/conways-game-of-life/index.ts
@@ -1,72 +1,71 @@
-const updatedStatus = (x: number, y: number, cell: number, sum: number) => {
-    if (cell === 1 && sum > 3) {
+const updateCellStatus = (x: number, y: number, currentCellStatus: number, neighbourSum: number) => {
+    if (currentCellStatus === 1 && neighbourSum > 3) {
         return 0;
     }
-    else if (cell === 1 && sum < 2) {
+    else if (currentCellStatus === 1 && neighbourSum < 2) {
         return 0;
     }
-    else if (cell === 0 && sum === 3) {
+    else if (currentCellStatus === 0 && neighbourSum === 3) {
         return 1;
     }
     else {
-        return cell;
+        return currentCellStatus;
     }
-} //Apply the conditions for updating status of a cell 
+} //Apply the conditions for updating the status of a cell 
 
-const calcSum = (input: number[][], x: number, y: number) => {
+const calculateNeighbourSum = (grid: number[][], x: number, y: number) => {
     let toBeSummed: number[] = [
-        input[y-1]?.[x+1],
-        input[y]?.[x+1],
-        input[y+1]?.[x+1],
-        input[y+1]?.[x],
-        input[y+1]?.[x-1],
-        input[y]?.[x-1],
-        input[y-1]?.[x-1],
-        input[y-1]?.[x]   
+        grid[y-1]?.[x+1],
+        grid[y]?.[x+1],
+        grid[y+1]?.[x+1],
+        grid[y+1]?.[x],
+        grid[y+1]?.[x-1],
+        grid[y]?.[x-1],
+        grid[y-1]?.[x-1],
+        grid[y-1]?.[x]   
     ];
     const toBeSummedClean = toBeSummed.filter(value => value !== undefined);
-    let sum = 0;
+    let neighbourSum = 0;
     for (const value of toBeSummedClean) {
-        sum += value;    
+        neighbourSum += value;    
     }
-    return sum;    
+    return neighbourSum;    
 } // Calculates and return the sum of neighbors alive around the cell of interest
 
 
-const calculateGen = (input: number[][]) => {
-    let result: number[][] = input.map(row => [...row]);
-    input.forEach((row, rowIndex) => {
+const calculateNextGeneration = (grid: number[][]) => {
+    let nextGeneration: number[][] = grid.map(row => [...row]);
+    grid.forEach((row, rowIndex) => {
         row.forEach((value, colIndex) => {
-            const cell = value;
+            const currentCellStatus = value;
             const y = rowIndex;
             const x = colIndex;
-            const sum: number = calcSum(input, x, y);
-            const newStatus: number = updatedStatus(x, y, cell, sum);
-            result[y][x] = newStatus;
+            const neighbourSum: number = calculateNeighbourSum(grid, x, y);
+            const newCellStatus: number = updateCellStatus(x, y, currentCellStatus, neighbourSum);
+            nextGeneration[y][x] = newCellStatus;
         });
     });
-    return result;
-}  // Calculates the next generation using the returns of functions calcSum() and updatedStatus()
+    return nextGeneration;
+}  // Calculates the next generation using the returns of functions calculateNeighbourSum() and updateCellStatus()
 
 function sleep(time: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
 
-const main = (input: number[][]) => {
-    //const gridSize = getGridSize(input);
-    let initialGeneration = [...input];
+const main = (grid: number[][]) => {
+    let initialGeneration = [...grid];
     console.log("Generation 0:");
     console.log(initialGeneration);
     async function startLoop() {
-        let i = 1;
+        let generationCount = 1;
         while (true) {
-            const nextGeneration = calculateGen(initialGeneration);
-            console.log("Generation " + i + ":");
+            const nextGeneration = calculateNextGeneration(initialGeneration);
+            console.log("Generation " + generationCount + ":");
             console.log(nextGeneration);
             initialGeneration = nextGeneration;
             await sleep(1000);
-            i++;
+            generationCount++;
         }
     }
     startLoop();
